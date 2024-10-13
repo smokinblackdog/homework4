ed7d09b51b1935148858116ef2719d8f8804ce49
smokinblackdog <iraagreg@gmail.com>
2024-10-13 22:27:40 +0300

    Initial commit

---
+++ hello.pas
@@ -0,0 +1,9 @@
+﻿begin
+  var lang:= ReadString('Ваш язык:');
+  case lang of
+    'русский': Print('Привет, мир!');
+    'английский': Print('Hello, world!');
+    'японский': Print('世界の皆さん、こんにちは！');
+    else Print('Язык не поддерживается.');
+  end;
+end.
